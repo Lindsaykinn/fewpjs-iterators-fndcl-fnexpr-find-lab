9757f43df69a2493d507119857a5e12668eaf104
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-03-03 14:20:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array){
+  let win = array.find(function(game){
+    return game.result === "W"})
+    if (!!win){
+      return win.year
+    } else {
+      return undefined
+    }
+}
